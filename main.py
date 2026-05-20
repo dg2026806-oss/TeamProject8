daaf23c6d11d0d34d7ff77e8045e7f713469aaa9
dg2026806-oss <dg2026806@danggok.hs.kr>
2026-05-20 11:13:36 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 Web VPython 3.2
 
-box(pos = vec (0,0,-25), size = vec (20,0.1,60), texture = textures.stucco)
-b = sphere(pos = vec (0,0.5,0), size = vec (1,1,1), color = vec (0,0,1))
+box(pos = vec (0,75,-45), size = vec (260,5,5), texture = textures.stucco)
+box(pos = vec (0,-75,-45), size = vec (260,5,5), texture = textures.stucco)
+box(pos = vec (130,0,-45), size = vec (5,155,5), texture = textures.stucco)
+box(pos = vec (-130,0,-45), size = vec (5,155,5), texture = textures.stucco)
+box(pos = vec (-110,60,-45), size = vec (5,25,5), texture = textures.stucco)
+box(pos = vec (-110,60,-45), size = vec (5,25,5), texture = textures.stucco)
+b = sphere(pos = vec (-120,55,-45), size = vec (5,5,5), color = vec (0,0,1))
 while True :
     rate(100)
     k = keysdown()
-    if 'd' in k :
-      b.pos.x = b.pos.x + 0.1
-    if 'a' in k :
-      b.pos.x = b.pos.x - 0.1
-    if 'w' in k :
-      b.pos.z = b.pos.z - 0.1
-    if 's' in k :
-      b.pos.z = b.pos.z + 0.1
+    if 'd' in k and b.pos.x > -125:
+      b.pos.x = b.pos.x + 0.4
+    if 'a' in k and b.pos.x > -125:
+      b.pos.x = b.pos.x - 0.4
+    if 'w' in k and b.pos.y > 10:
+      b.pos.y = b.pos.y + 0.4
+    if 's' in k and b.pos.y > -70:
+      b.pos.y = b.pos.y - 0.4
