9f96b1061f7d61df00650f00fd80fa5808782340
dg2026806-oss <dg2026806@danggok.hs.kr>
2026-05-22 09:09:17 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,25 @@
 Web VPython 3.2
 
-box(pos = vec (0,75,-45), size = vec (260,5,5), texture = textures.stucco)
-box(pos = vec (0,-75,-45), size = vec (260,5,5), texture = textures.stucco)
-box(pos = vec (130,0,-45), size = vec (5,155,5), texture = textures.stucco)
-box(pos = vec (-130,0,-45), size = vec (5,155,5), texture = textures.stucco)
-box(pos = vec (-110,60,-45), size = vec (5,25,5), texture = textures.stucco)
-box(pos = vec (-110,60,-45), size = vec (5,25,5), texture = textures.stucco)
-b = sphere(pos = vec (-120,55,-45), size = vec (5,5,5), color = vec (0,0,1))
+box(pos = vec (0,-10,0), size = vec (260,5,60), texture = textures.stucco)
+box(pos = vec (-90,0,0), size = vec (10,15,10), texture = textures.stucco)
+box(pos = vec (-60,0,0), size = vec (10,25,10), texture = textures.stucco)
+box(pos = vec (-30,5,0), size = vec (10,35,10), texture = textures.stucco)
+box(pos = vec (0,25,0), size = vec (20,5,20), texture = textures.stucco)
+box(pos = vec (60,25,0), size = vec (20,5,20), texture = textures.stucco)
+box(pos = vec (90,25,0), size = vec (10,5,10), texture = textures.stucco)
+box(pos = vec (120,35,0), size = vec (20,5,20), texture = textures.stucco)
+box(pos = vec (90,55,0), size = vec (20,5,20), texture = textures.stucco)
+box(pos = vec (10,55,0), size = vec (90,5,20), texture = textures.stucco)
+cone(pos = vec (10,25,0), size = vec (60,10,10), color = vec (1,0,0), rotate = vec (0,0,0))
+b = sphere(pos = vec (-120,-5,0), size = vec (5,5,5), color = vec (0,0,1))
 while True :
     rate(100)
     k = keysdown()
-    if 'd' in k and b.pos.x > -125:
+    if 'd' in k:
       b.pos.x = b.pos.x + 0.4
-    if 'a' in k and b.pos.x > -125:
+    if 'a' in k:
       b.pos.x = b.pos.x - 0.4
-    if 'w' in k and b.pos.y > 10:
+    if 'w' in k:
       b.pos.y = b.pos.y + 0.4
-    if 's' in k and b.pos.y > -70:
+    if 's' in k:
       b.pos.y = b.pos.y - 0.4
