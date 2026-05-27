67fe5694f964aa78cf93b30e407cea12b1570fdb
dg2026806-oss <dg2026806@danggok.hs.kr>
2026-05-27 11:11:29 +0900

    Update main.py

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ box(pos = vec (90,55,0), size = vec (20,5,20), texture = textures.stucco)
 box(pos = vec (10,55,0), size = vec (90,5,20), texture = textures.stucco)
 cone(pos = vec (10,25,0), size = vec (60,10,10), color = vec (1,0,0), rotate = vec (0,0,0))
 b = sphere(pos = vec (-120,-5,0), size = vec (5,5,5), color = vec (0,0,1))
+
 while True :
     rate(100)
     k = keysdown()
@@ -23,3 +24,4 @@ while True :
       b.pos.y = b.pos.y + 0.4
     if 's' in k:
       b.pos.y = b.pos.y - 0.4
+     
